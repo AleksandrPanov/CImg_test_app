f11848c2f43eb2d4b3101eb0268e00324975d9df
AleksandrPanov <sasha1_1997@mail.ru>
2020-02-21 22:55:07 +0300

    added BackgroundSearcher

--- src/test2.cpp
+++ src/test2.cpp
@@ -1,57 +1,113 @@
 #include "CImg.h"
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace cimg_library;
-void setPixel(const CImg<unsigned char> &img, int x, int y, CImg<unsigned char> &res)
-{
-    res(x, y) = img(x, y);
-    res(x, y) = img(x, y, 0, 1);
-    res(x, y) = img(x, y, 2);
-}
+
 struct Point
 {
-    int x = 0;
-    int y = 0;
-    Point(int x, int y): x(x), y(y){}
+    int x;
+    int y;
+    Point():x(0), y(0){}
+    Point(int x, int y) : x(x), y(y) {}
 };
-bool setNeighbors(Point coord, Point size, std::vector<bool> &was, std::vector<Point> &neighbors)
+struct BackgroundSearcher
 {
-    //
-    return neighbors.size() > 0;
-}
-std::vector<Point> search(const CImg<unsigned char> &img, Point coord)
-{
-    unsigned char red = img(coord.x, coord.y);
-    Point size(img.width(), img.height());
-    std::vector<Point> neighbors, res;
+    const CImg<unsigned char> &img;
+    Point startPixel;
+    Point size;
     std::vector<bool> was;
-    while (setNeighbors(coord, size, was, neighbors))
+    std::vector<Point> res, neighbors;
+    BackgroundSearcher(const CImg<unsigned char> &img): img(img), size(img.width(), img.height())
     {
-        coord = neighbors.back();
-        res.push_back(coord);
-        neighbors.pop_back();
+        was = std::vector<bool>(size.x*size.y);
     }
-    return res;
+    int getDistance(Point pixel1, Point pixel2)
+    {
+        return std::abs(img(pixel1.x, pixel1.y) - img(pixel2.x, pixel2.y))
+            + std::abs(img(pixel1.x, pixel1.y, 0, 1) - img(pixel2.x, pixel2.y, 0, 1))
+            + std::abs(img(pixel1.x, pixel1.y, 2) - img(pixel2.x, pixel2.y, 2));
+    }
+    int getDistance(Point pixel)
+    {
+        return getDistance(startPixel, pixel);
+    }
+    bool inside(Point a)
+    {
+        return (a.x >= 0 && a.x < size.x && a.y >= 0 && a.y < size.y);
+    }
+    int getIndex(Point pixel)
+    {
+        return pixel.y * size.x + pixel.x;
+    }
+    bool isTruePixel(Point prev_pixel, Point pixel, int threshold = 3)
+    {
+        return (inside(pixel) && !was[getIndex(pixel)] && getDistance(prev_pixel, pixel) <= threshold);
+    }
+    bool isTruePixel(Point pixel, int threshold = 50)
+    {
+        return isTruePixel(startPixel, pixel, threshold);
+    }
+    void push(Point pixel)
+    {
+        was[getIndex(pixel)] = true;
+        res.push_back(pixel);
+        neighbors.push_back(pixel);
+    }
+    void setNeighbors(Point prevPixel)
+    {
+        Point a1(prevPixel.x - 1, prevPixel.y), a2(prevPixel.x, prevPixel.y - 1), a3(prevPixel.x + 1, prevPixel.y), a4(prevPixel.x, prevPixel.y + 1);
+        if (isTruePixel(a1))
+            push(a1);
+        if (isTruePixel(a2))
+            push(a2);
+        if (isTruePixel(a3))
+            push(a3);
+        if (isTruePixel(a4))
+            push(a4);
+    }
+    std::vector<Point>& search(Point _startPixel)
+    {
+        startPixel = _startPixel;
+        push(startPixel);
+        int count = 0;
+        while (count < neighbors.size())
+        {
+            Point pixel = neighbors[count++];
+            setNeighbors(pixel);
+        }
+        return res;
+    }
+};
+void setPixel(const CImg<unsigned char> &img, Point p, CImg<unsigned char> &res)
+{
+    res(p.x, p.y) = img(p.x, p.y);
+    res(p.x, p.y, 0, 1) = img(p.x, p.y, 0, 1);
+    res(p.x, p.y, 2) = img(p.x, p.y, 2);
 }
+
 
 int main()
 {
-    unsigned char purple[] = { 255,0,255 };
-    CImg<unsigned char> image("girl.bmp"), res(image.width(), image.height());
-    res.fill(255);
+    CImg<unsigned char> image("girl.bmp"), res(image.width(), image.height(), 1, 3);
+    res.fill(255, 255, 255);
     const int w = image.width() / 5, h = image.height() / 5;
     CImgDisplay main_disp(w, h, "Click a point"), draw_disp(w, h, "Res");
+    image.blur(2.5);
     main_disp.display(image);
     draw_disp.display(res);
+    BackgroundSearcher searcher(image);
     while (!main_disp.is_closed() && !draw_disp.is_closed())
     {
         main_disp.wait();
         if (main_disp.button() && main_disp.mouse_y() >= 0 && main_disp.mouse_x() >= 0) {
             const int x = main_disp.mouse_x() * image.width() / (double)(main_disp.width());
             const int y = main_disp.mouse_y() * image.height() / (double)(main_disp.height());
-            setPixel(image, x, y, res);
+            auto pixs = searcher.search(Point(x, y));
+            std::cout << x << " " << y << " " << pixs.size() << "\n";
+            for (auto pix : pixs)
+                setPixel(image, pix, res);
             draw_disp.display(res);
-            std::cout << x << " " << y << " " << "\n";
         }
     }
     return 0;
